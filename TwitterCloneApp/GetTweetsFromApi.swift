9f55b53a6d5aa08066ff9b719b54a886320441cc
Shohei Shikanai <shikanai1993@gmail.com>
2020-12-22 23:02:11 +0900

    make new file

---
+++ TwitterCloneApp/GetTweetsFromApi.swift
@@ -0,0 +1,14 @@
+//
+//  GetTweetsFromApi.swift
+//  TwitterCloneApp
+//
+//  Created by 鹿内翔平 on 2020/12/22.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class GetTweetsFromApi {
+    
+}
